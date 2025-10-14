5330d1c3c138ba059bb0731ec76b32a823416386
jorgelcg <jorge.cardenastp@gmail.com>
2025-10-14 14:53:59 -0500

    adding service

--- CclService/Services/ControlService .cs
+++ CclService/Services/ControlService .cs
@@ -5,27 +5,12 @@ namespace CclService.Services
 {
     public class ControlService : IControlservice
     {
-        public Task<ProductDto> CreateAsync(PoductRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<ProductDto> CreateAsync(ProductDto.Producs request)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task DeleteAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<ProductDto>> GetAllAsync(CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
